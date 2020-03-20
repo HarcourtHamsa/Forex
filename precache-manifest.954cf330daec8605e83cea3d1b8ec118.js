adb748560a22b74fde7ea8427c06ff58a66fa444
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-03-20 09:21:44 +0100

    Updates

---
+++ precache-manifest.954cf330daec8605e83cea3d1b8ec118.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958cefc7742f41598c74cb82d28fcbc6",
+    "url": "/Forex/index.html"
+  },
+  {
+    "revision": "72983f34849b1115d36f",
+    "url": "/Forex/static/css/main.0ea408d9.chunk.css"
+  },
+  {
+    "revision": "fe0191d6b425aa5c6f79",
+    "url": "/Forex/static/js/2.828c86ed.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/Forex/static/js/2.828c86ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72983f34849b1115d36f",
+    "url": "/Forex/static/js/main.296159d3.chunk.js"
+  },
+  {
+    "revision": "df3b928113dcc734cc11",
+    "url": "/Forex/static/js/runtime-main.73c68772.js"
+  },
+  {
+    "revision": "2634a84ee379331d614f31c3d2905f9c",
+    "url": "/Forex/static/media/instructions.2634a84e.svg"
+  },
+  {
+    "revision": "336ecdde5f7bea4b899dbf2dc7818d2e",
+    "url": "/Forex/static/media/ishant-mishra-UXEJDX4SqdE-unsplash.336ecdde.jpg"
+  },
+  {
+    "revision": "c1f04629b4d96cd32c3ba99eff151bbc",
+    "url": "/Forex/static/media/laptop_stock.c1f04629.png"
+  },
+  {
+    "revision": "e0b7a81618f0ff84cdc7e92a7936d3be",
+    "url": "/Forex/static/media/ledger.e0b7a816.svg"
+  },
+  {
+    "revision": "79a16ed77ebec2f21b0a8db46bfdabc3",
+    "url": "/Forex/static/media/mortarboard.79a16ed7.svg"
+  },
+  {
+    "revision": "7709129f006e37f3da5f2cc181f7b95e",
+    "url": "/Forex/static/media/presentation.7709129f.svg"
+  },
+  {
+    "revision": "623823043fcd45251e31a5ec1f537d0b",
+    "url": "/Forex/static/media/stock_img.62382304.jpg"
+  }
+]);
